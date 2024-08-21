145508e5670b3ec2fb80a1281ed98bf023e2ea64
Carlos Rangel <charlesleimaginant@gmail.com>
2024-08-20 19:49:42 -0600

    Database Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,16 @@
 import styles from "./page.module.css";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+
+  const session = await getServerSession();
+
+  if (!session) {
+    //redirect('/api/auth/signin');
+    return <p>You must be signed in...</p>
+  }
+
   return (
     <main className={styles.main}>
       
