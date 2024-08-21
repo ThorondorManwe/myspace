66cf501bf622b77d0d70f5630fffc35ea5a2726c
Carlos Rangel <charlesleimaginant@gmail.com>
2024-08-20 19:06:26 -0600

    Add Sign in/Sign out buttons

--- app/NavMenu.tsx
+++ app/NavMenu.tsx
@@ -1,6 +1,9 @@
 import Link from "next/link";
 import Image from "next/image";
 import styles from "./NavMenu.module.css";
+import { SignInButton, SignOutButton } from "@/components/buttons";
+import AuthCheck from "@/components/AuthCheck";
+import { Sign } from "crypto";
 
 export default function NavMenu() {
     return (
@@ -27,6 +30,16 @@ export default function NavMenu() {
 
                 <li>
                     <Link href={'/users'}>Users</Link>
+                </li>
+
+                <li>
+                    <SignInButton />
+                </li>
+
+                <li>
+                    <AuthCheck>
+                        <SignOutButton />
+                    </AuthCheck>
                 </li>
 
             </ul>
