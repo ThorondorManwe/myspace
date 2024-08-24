e16d67641d17295c77d50f4e4ac1d2798190393c
Carlos Rangel <charlesleimaginant@gmail.com>
2024-08-23 20:54:07 -0600

    Implement following features

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,11 @@
-import styles from "./page.module.css";
-import { getServerSession } from "next-auth";
-import { redirect } from "next/navigation";
-
-export default async function Home() {
-
-  const session = await getServerSession();
-
-  if (!session) {
-    //redirect('/api/auth/signin');
-    return <p>You must be signed in...</p>
-  }
-
+export default function Home() {
   return (
-    <main className={styles.main}>
-      
-    </main>
+    <div>
+      <h1>Welcome to NextSpace!</h1>
+      <p>
+        A next-gen social media app to connect with frens inspired by MySpace
+      </p>
+      <p>To get started, sign up for an account</p>
+    </div>
   );
 }
